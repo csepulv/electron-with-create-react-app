3ead0e99f60035d2f7c51a21b7b2b0aa1e10bcb8
Christian Sepulveda <csepulv@gmail.com>
2017-02-24 14:08:50 -0800

    example of using remote objects in electron

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const electron = window.require('electron');
+const fs = electron.remote.require('fs');
+const ipcRenderer = electron.ipcRenderer;
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React/Electron</h2>
-        </div>
-        <p className="App-intro">
-          Hello Electron!
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {files: []}
+    }
+
+    componentDidMount() {
+        fs.readdir('/', (err, files) => {
+            this.setState({files: files});
+        });
+
+        //ipcRenderer example, from http://electron.atom.io/docs/api/ipc-main/
+        console.log(ipcRenderer.sendSync('synchronous-message', 'ping')); // prints "pong"
+
+        ipcRenderer.on('asynchronous-reply', (event, arg) => {
+            console.log(arg);// prints "pong"
+        });
+        ipcRenderer.send('asynchronous-message', 'ping');
+    }
+
+    renderFiles = () => {
+        return this.state.files.map((file, index) => {
+            return (<p key={index}>{file}</p>);
+        })
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React/Electron</h2>
+                </div>
+                <p className="App-intro">
+                    Hello Electron!
+                </p>
+                {this.renderFiles()}
+
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/electron-starter.js
+++ src/electron-starter.js
@@ -7,6 +7,18 @@ const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
 
+//  Example of IPC Renderer, http://electron.atom.io/docs/api/ipc-main/
+const {ipcMain} = require('electron');
+ipcMain.on('asynchronous-message', (event, arg) => {
+    console.log(arg);  // prints "ping"
+    event.sender.send('asynchronous-reply', 'pong')
+});
+
+ipcMain.on('synchronous-message', (event, arg) => {
+    console.log(arg); // prints "ping"
+    event.returnValue = 'pong'
+});
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
