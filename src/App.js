90ff40f1b9622fa137e1f1ef4a1887f00cfaf5d3
Kara Marie Rawson <rawsonkara@gmail.com>
2017-09-18 10:16:36 -0400

    imported changes to use npm start over npm run dev

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React/Electron</h2>
+          <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Hello Electron!
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
