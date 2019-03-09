be70827bcf9ba00f908a3f17691cdb29ce53065b
Joel Bellot <joelbellot@joels-mbp.mynet>
2019-03-09 09:54:21 +0000

    update packages. dev (not build) works mac

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React/Electron</h2>
         </div>
         <p className="App-intro">
-          Hello Electron!
+          Hello Electron
         </p>
       </div>
     );
