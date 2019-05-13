ff827ee0d21f3e0bc8817c848a8de02622150651
Joel Bellot <joelbellot@joels-mbp.mynet>
2019-05-13 05:54:07 +0100

    can get dialog when running npm run dev, not when only react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+//import remote from 'electron';
+const electron = window.require("electron");
+const remote = window.require('electron').remote;
+const dialog = remote.require('electron').dialog;
+//const fs = require('fs');
+
 
 class App extends Component {
   constructor() {
     super();
 
     this.handleClick = this.handleClick.bind(this);
+    this.uploadFileButton = this.uploadFileButton.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   state = {
@@ -17,6 +25,15 @@ class App extends Component {
     //fetch("http://localhost:5000/api/contacts")
     //      .then(response => response.json())
     //.then(data => this.setState({ data }));
+  }
+
+  handleChange(event) {
+    //this.setState({ value: event.target.value });
+    console.log(event.target.value);
+    var f = document.getElementById("myFile");
+    console.log(f);
+    f = document.getElementById("myFile2");
+    console.log(f);
   }
 
   render() {
@@ -35,16 +52,23 @@ class App extends Component {
         <button className="button" onClick={this.handleClick}>
           Click Me
         </button>
+        <input id="myFile" type="file" value={this.state.value} onChange={this.handleChange} />
       </div>
     );
   }
 
   handleClick() {
+    console.log(dialog.showOpenDialog({ properties: ['openFile', 'openDirectory', 'multiSelections'] }));
+    return;
     fetch(
       "http://localhost:5000/api/imposition/6/6/%2FUsers%2Fjoelbellot%2FDocuments%2Fgit%2Ftetefufu%2Fimposition%2Fapi%2Fbin%2Fdist%2Fosx%2Fa4.pdf"
     )
       .then(response => response.json())
       .then(data => console.log(data));
+  }
+
+  uploadFileButton() {
+    return;
   }
 }
 
--- src/electron-starter.js
+++ src/electron-starter.js
@@ -37,7 +37,7 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', startApi);
+app.on('ready', createWindow);
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
