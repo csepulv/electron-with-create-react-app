5f76f3a426406c5f79d18b59f8f418fa523bc75a
Christian Sepulveda <csepulv@gmail.com>
2019-05-24 09:45:50 -0700

    tweak, for nodeIntegration
    as recommended by https://github.com/csepulv/electron-with-create-react-app/issues/30

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -13,7 +13,7 @@ let mainWindow;
 
 function createWindow() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences: { nodeIntegration: true }});
 
     // and load the index.html of the app.
     const startUrl = process.env.ELECTRON_START_URL || url.format({
